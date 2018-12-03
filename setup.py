9d4dd6f69ced65a424a653edd0e16549b0cb4c8d
Sergey Ivanov <s.ivanov@onetrak.ru>
2018-12-03 13:39:22 +0300

    Add new version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-tinkoff',
-    version='0.43',
+    version='0.44',
     author='Ivanov S',
     author_email='nameoff.nv@gmail.com',
     packages=['django_tinkoff_merchant'],
